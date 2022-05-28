87e9bf979ace994d54467d3b49c635f92bdeb4d6
+ <+>
2022-05-28 18:50:45 -0400

    Third Commit Light Dark Mode JS

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # My Personal Website
+
+
+In order to create a light mode/dark mode functionality, I created a floating button using two icons, and then used JavaScript to make this button change between two css files - one light mode, and one dark mode. 
--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const icon = document.getElementById("mode-icon");
 
+icon.addEventListener('click', function() {
+    const css = document.getElementById("css-file");
+    const modeIcon = document.getElementById("mode-icon");
+
+    if (modeIcon.className === "dark-mode") {
+        css.setAttribute("href", "./light-mode.css");
+        modeIcon.setAttribute("src", "./Theme-Icons/moon.svg");
+        modeIcon.setAttribute("class", "light-mode");
+    } else {
+        css.setAttribute("href", "./dark-mode.css");
+        modeIcon.setAttribute("src", "./Theme-Icons/sun.svg");
+        modeIcon.setAttribute("class", "dark-mode");
+    }
+
+
+});
